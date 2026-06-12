22187f48a68e4d0f874523310241c9878fd041bd
Joshua Cabuang <joshuacabuang0@gmail.com>
2026-06-12 15:55:30 +0800

    practice

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,9 +1,11 @@
 import ControlledComponent from "./components/ControlledComponent"
 import Form from "./components/FeedbackForm"
+import Register from "./components/PracticeForm"
+
 function App() {
   return (
     <>
-      <Form />
+      <Register />
     </>
   )
 }
