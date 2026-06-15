a6d5944913667e15870fa3c1e7a7a42e22a5ef9a
Joshua Cabuang <joshuacabuang0@gmail.com>
2026-06-15 18:49:53 +0800

    custom hooks

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -5,12 +5,12 @@ import Welcome from "./pages/WelcomePage";
 import GiftCard from "./components/ManagingState";
 import UseEffect from "./components/UseEffect";
 import FetchPost from "./components/Fetch";
-
+import CustomHook from "./components/CustomHook";
 
 function App() {
   return (
     <>
-      <FetchPost />
+      <CustomHook />
     </>
   )
 }
