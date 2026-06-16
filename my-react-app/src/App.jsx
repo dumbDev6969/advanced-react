69ffbfb1b042b8c1880fd1a563737a9cc56df540
Joshua Cabuang <joshuacabuang0@gmail.com>
2026-06-16 10:26:03 +0800

    children manipulation

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,3 +1,4 @@
+
 import ControlledComponent from "./components/ControlledComponent";
 import Form from "./components/FeedbackForm";
 import Register from "./components/PracticeForm";
@@ -8,15 +9,24 @@ import FetchPost from "./components/Fetch";
 import CustomHook from "./components/CustomHook";
 import Alert from "./components/containmentAndSpecialization/Alert";
 import DeleteButton from "./components/containmentAndSpecialization/DeleteButton";
+import { RadioGroup, RadioOption } from "./components/radio/index";
+import { useState } from "react";
 
 function App() {
+  const [selected, setSelected] = useState("");
+
   return (
     <>
-      <Alert>
-        <h1>Delete account</h1>
-        <p>Are you sure you want to delete your account?</p>
-        <DeleteButton backgroundColor="red">Delete</DeleteButton>
-      </Alert>
+      <div className="App">
+        <h2>How did you hear about Little Lemon?</h2>
+        <RadioGroup onChange={setSelected} selected={selected}>
+          <RadioOption value="social_media">Social Media</RadioOption>
+          <RadioOption value="friends">Friends</RadioOption>
+          <RadioOption value="advertising">Advertising</RadioOption>
+          <RadioOption value="other">Other</RadioOption>
+        </RadioGroup>
+        <button disabled={!selected}>Submit</button>
+      </div>
     </>
   )
 }
